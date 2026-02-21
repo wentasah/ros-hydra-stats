6817e257aa7dbfbbf6c54187ac874cbb71f428f3
Michal Sojka <michal.sojka@cvut.cz>
2026-02-22 00:13:45 +0100

    Allow comparing jobsets

--- src/cli.rs
+++ src/cli.rs
@@ -14,11 +14,21 @@ pub enum Commands {
     /// Produce comparison of PR source and target
     PR { pr: usize },
     /// Produce comparison of two evaluations
-    Compare {
+    CompareEvals {
         /// Old eval id
         old: u64,
         /// New eval id
         new: u64,
+    },
+    /// Produce latest evaluations of nix-ros-exoeriments jobsets
+    CompareJobsets {
+        /// Old jobset (e.g. lopsided98-master)
+        old: String,
+        /// New jobset (e.g. lopsided98-develop)
+        new: String,
+        /// Whether to use cached evaluation data (if available) instead of fetching from hydra
+        #[arg(long, short = 'c', default_value_t = false)]
+        use_cache: bool,
     },
 }
 
